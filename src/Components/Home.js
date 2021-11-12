7325e98fff3b6ace551b17bf5318c6b2781be1b6
Euthyphro <lulu.cao.her@gmail.com>
2021-11-12 13:06:11 -0600

    update home.js nav, header, footer

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -30,6 +30,45 @@ function Home() {
     <form method="post">
         <input type="button" value="SEND CRISIS ALERT" onClick={()=>{sendAlert()}}/>
         </form>
+
+
+        <main className="main" role="main">
+    <div className="bg-white">
+      <div className="container-fluid p-0">
+        <div className="row no-gutters vh-100">
+          <div className="col-12 col-sm-6 order-sm-2 bg-white d-flex justify-content-center align-items-center bg-overlay-teal">
+            <img src="_images/cover.jpg" className="img-fluid img-cover" alt="Cultivate Community" />
+          </div>
+          <div className="col-12 col-sm-6 order-sm-1 bg-light d-flex position-relative justify-content-center align-items-center">
+            <div className="p-3 p-md-4 p-lg-8 w-100">
+              <div className="card">
+                <div className="card-body">
+                  <h2 className="display-4 mb-2">Welcome!</h2>    
+                  <div className="m-sm-4">
+                    <form>
+                      <div className="form-group">
+                        <label>Username</label>
+                        <input className="form-control form-control-lg" type="text" name="username" placeholder="Enter your username" />
+                      </div>
+                      <div className="form-group">
+                        <label>Password</label>
+                        <input className="form-control form-control-lg" type="password" name="password" placeholder="Enter password" />
+                      </div>
+                      <div className="text-center mt-3">
+                        <a href="myaccount.html" className="btn btn-lg btn-primary">Log In</a>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+          
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 <Footer />
 </div>
 )
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -8,14 +8,14 @@ function Nav(props) {
   <div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="./_images/logo.png"/></a>
+        <a class="navbar-brand" href="#"><img src="./_images/logo.png" alt="Cultivate Community"/></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="./landingpage.html">Home</a>
+              <a class="nav-link" aria-current="page" href="./loginpage.html">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" aria-current="page" href="./aboutpage.html">About</a>
