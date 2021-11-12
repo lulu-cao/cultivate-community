904f2f47abab674c9f95b495641861e4a0187955
Katrina Behnken <katrina.behnken@gmail.com>
2021-11-12 00:47:50 -0700

    Add routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 // import ManageCareTeam from './Components/ManageCareTeam';
+import Home from './Components/Home';
 import Resources from './Components/Resources';
+import Resource2 from './Components/Resource2';
 import Login from './Components/Login';
 import Logout from './Components/Logout';
 // import PrivateRoutes from './Components/PrivateRoutes';
@@ -10,7 +12,9 @@ export default (
     <Routes>
         {/* <PrivateRoute exact path='/' component={Home} /> */}
         {/* <PrivateRoutes path='/manage-care-team' element={<ManageCareTeam />} /> */}
-        {/* <Route path='/resources' element={<Resources />} /> */}
+        <Route path='/home' element={<Home />} />
+        <Route path='/resources' element={<Resources />} />
+        <Route path='/resource2' element={<Resource2 />} />
         <Route path='/login' element={<Login />} />
         {/* <Route path='/logout' element={<Logout />} /> */}
     </Routes>
