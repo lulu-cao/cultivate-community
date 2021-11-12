8d5d83dc4efe2d475f13ad03ac0ebf8152ebc7f7
Jillian Sechrest <jillian.sechrest@bewaterwsnc.com>
2021-11-12 09:03:33 -0500

    landing shell

---
+++ src/Components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import Nav from './Nav';
+import '../html-css/css/app.css';
+import '../html-css/css/resource.css'
+
+function Landing(props) {
+    return (
+        <div>
+            <Nav />
+            Need html
+        </div>
+    )
+}
+export default Landing;
