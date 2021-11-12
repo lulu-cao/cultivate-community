1d094dda68fb872a00772b57f5795aa247f8607b
Euthyphro <lulu.cao.her@gmail.com>
2021-11-12 11:45:23 -0600

    update footer and header

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -6,25 +6,25 @@ function Nav(props) {
     
   //This is the area where you paste your HTML codes
   <div>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#"><img src="_images/logo.png"/></a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="_images/logo.png"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="#">Home</a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="./landingpage.html">Home</a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="#">About</a>
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="./aboutpage.html">About</a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Resources</a>
+            <li class="nav-item">
+              <a class="nav-link" href="./resourcepage.html">Resources</a>
             </li>
           </ul>
-          <button className="btn btn-outline-success" type="submit">Login</button>
+          <button class="btn btn-outline-success" type="submit"><a href="loginpage.html" style="color:black; text-decoration: none;">Login</a></button>
         </div>
       </div>
     </nav>
