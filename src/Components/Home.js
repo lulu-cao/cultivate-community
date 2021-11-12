5e8e0aaf7dab74997c323c9d8970ec72b150a767
Jillian Sechrest <jillian.sechrest@bewaterwsnc.com>
2021-11-12 12:20:45 -0500

    home sendalert app

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,51 +1,35 @@
-import Header from './Components/Header';
-import Footer from './Components/Footer'; 
-import React, { Component } from 'react'; 
+import Header from './Header';
+import Footer from './Footer'; 
+import React from 'react'; 
 
-class Home extends Component {
 
-    sendAlert() {
-        Email.send({
+function Home() {
+
+    const sendAlert = () => {
+       Alert.send({
             Host: "smtp.gmail.com",
             Username: "emergency.cultivatecommunity@bewaterwsnc.com",
             Password: "genericP@$$WORD",
             To: "jillian.sechrest@bewaterwsnc.com",
             From: "emergency.cultivatecommunity@bewaterwsnc.com",
             Body: "***CARE WEB EMERGENCY - IMMEDIATE ASSISTANCE REQUIRED: -USERS GELOCATION FROM BROWSER-",   
-        }).then(
-            message => alert("Alert Sent Successfully")
-        )
-    }
+        }).then(message => alert("Alert Sent Successfully"))
 
-    render() {
 
-        return (
+      return (
 
 //This is the area where you paste your HTML codes
 <div>
 <Header />
-<body>
-<script src="https://smtpjs.com/v3/smtp.js"></script>
-   <script src="index.js"></script>
 <div>
+<script src="https://smtpjs.com/v3/smtp.js"></script>
+</div>
     <form method="post">
-        <input type="button" value="SEND CRISIS ALERT" onClick="sendAlert()"/>
+        <input type="button" value="SEND CRISIS ALERT" onClick={()=>{sendAlert()}}/>
         </form>
-        </div>
-        </body>
 <Footer />
 </div>
 )
-
+      }
     }
-
-}
-
 export default Home;
-
-/*geo = navigator.geolocation
-    if ('geolocation' in navigator) {
-}
-    else {
-
-    }*/
