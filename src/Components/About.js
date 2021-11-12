c4d29758902ca67ed8bbfbcb9451a2e824e7dbdb
Katrina Behnken <katrina.behnken@gmail.com>
2021-11-12 13:28:51 -0700

    Importer Footer component

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,5 +1,6 @@
 import React from 'react'; 
 import Nav from './Nav';
+import Footer from './Footer';
 
 function About(props) {
   return (
--- src/Components/Resources.js
+++ src/Components/Resources.js
@@ -1,4 +1,5 @@
 import Nav from './Nav'
+import Footer from './Footer'
 
 function Resources(props) {
     return (
