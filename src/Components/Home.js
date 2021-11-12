3b74e2b341d78d9c63281c288ed2cf71f73d5d53
Jillian Sechrest <jillian.sechrest@bewaterwsnc.com>
2021-11-11 21:49:17 -0500

    add sendAlert() to Home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,6 +4,19 @@ import React, { Component } from 'react';
 
 class Home extends Component {
 
+    sendAlert() {
+        Email.send({
+            Host: "smtp.gmail.com",
+            Username: "emergency.cultivatecommunity@bewaterwsnc.com",
+            Password: "genericP@$$WORD",
+            To: "jillian.sechrest@bewaterwsnc.com",
+            From: "emergency.cultivatecommunity@bewaterwsnc.com",
+            Body: "***CARE WEB EMERGENCY - IMMEDIATE ASSISTANCE REQUIRED: -USERS GELOCATION FROM BROWSER-",   
+        }).then(
+            message => alert("Alert Sent Successfully")
+        )
+    }
+
     render() {
 
         return (
