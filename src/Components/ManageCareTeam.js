dc4c951f98d8e0619daec16f33a2d28705b18aac
Katrina Behnken <katrina.behnken@gmail.com>
2021-11-14 09:08:16 -0700

    Add AddCareMember component

--- src/Components/ManageCareTeam.js
+++ src/Components/ManageCareTeam.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import AddCareMember from './AddCareMember';
 
 function ManageCareTeam(props) {
     return (
         <div>
             Care Team list goes here.
+            <AddCareMember />
         </div>
     )
 }
