9f071f237d5311b9adbe2d0347204d092aeb119e
Katrina Behnken <katrina.behnken@gmail.com>
2021-11-14 08:03:30 -0700

    Add reducers to request, add, and update members

---
+++ src/Redux/Reducers/careMemberReducer.js
@@ -0,0 +1,45 @@
+import { careMemberConsts } from '../Consts/careMemberConsts';
+
+const initialState = {
+    careMember: {firstName: '', lastName: '', smsAddress: ''},
+    careMembers: [],
+    loading: false
+};
+
+export default function reducer(previousState = initialState, action) {
+    switch(action.type) {
+        case careMemberConsts.REQUEST_CARE_MEMBERS_PENDING:
+            return ({
+                ...previousState,
+                loading: true
+            });
+        case careMemberConsts.REQUEST_CARE_MEMBERS_SUCCESS:
+            return ({
+                ...previousState,
+                loading: false,
+                careMembers: action.payload
+            });
+        case careMemberConsts.ADD_CARE_MEMBER_PENDING:
+            return ({
+                ...previousState,
+                loading: true
+            });
+        case careMemberConsts.ADD_CARE_MEMBER_SUCCESS:
+            return ({
+                ...previousState,
+                loading: false
+            });
+        case careMemberConsts.UPDATE_CARE_MEMBER + '_PENDING':
+            return ({
+                ...previousState,
+                loading: true
+            });
+        case careMemberConsts.UPDATE_CARE_MEMBER + '_FULFILLED':
+            return ({
+                ...previousState,
+                loading: false,
+                careMembers: action.payload.data
+            });
+        default: return (previousState);
+    }
+}
