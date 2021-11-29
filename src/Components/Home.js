3b6d8852c316516a92548484770c043fad17c020
Katrina Behnken <katrina.behnken@gmail.com>
2021-11-29 11:29:40 -0700

    Fix comment tag

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -68,10 +68,10 @@ export default Home;
     //     }).then(message => alert("Alert Sent Successfully"))
     // }
 
-{/* <script src="https://smtpjs.com/v3/smtp.js"></script>
-    <form method="post">
-        <input type="button" value="SEND CRISIS ALERT" onClick={()=>{sendAlert()}}/>
-        </form> */}
+// <script src="https://smtpjs.com/v3/smtp.js"></script>
+    // <form method="post">
+    //     <input type="button" value="SEND CRISIS ALERT" onClick={()=>{sendAlert()}}/>
+    //     </form>
 
 // const Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
     
